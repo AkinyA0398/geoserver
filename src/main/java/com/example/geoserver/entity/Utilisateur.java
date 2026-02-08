1b9a1a0843839cdb8b5102e73afc51d088666195
AkinyA0398 <nyaraja398@gmail.com>
2026-02-08 11:50:09 +0300

    Create Utilisateur.java

---
+++ src/main/java/com/example/geoserver/entity/Utilisateur.java
@@ -0,0 +1,33 @@
+package com.example.geoserver.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "utilisateur")
+@Data
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Column(nullable = false)
+    private String nom;
+    
+    private String photo;
+    
+    @Column(name = "failed_attempts")
+    private Integer failedAttempts = 0;
+    
+    private Boolean blocked = false;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_entreprise")
+    private Entreprise entreprise;
+}
