08acf8f106c29e66babe57ca58621d733b649e61
AkinyA0398 <nyaraja398@gmail.com>
2026-02-08 11:49:50 +0300

    Create Entreprise.java

---
+++ src/main/java/com/example/geoserver/entity/Entreprise.java
@@ -0,0 +1,23 @@
+package com.example.geoserver.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Table(name = "entreprise")
+@Data
+public class Entreprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String nom;
+    
+    @OneToMany(mappedBy = "entreprise")
+    private List<Utilisateur> utilisateurs;
+    
+    @OneToMany(mappedBy = "entreprise")
+    private List<Signalement> signalements;
+}
