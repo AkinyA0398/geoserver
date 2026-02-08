1936d1fb99a0b8bc09b7e9fd27ecee199883a5f4
AkinyA0398 <nyaraja398@gmail.com>
2026-02-08 14:56:57 +0300

    Update SignalementRepository.java
    liste des signalements valides

--- src/main/java/com/example/geoserver/repository/SignalementRepository.java
+++ src/main/java/com/example/geoserver/repository/SignalementRepository.java
@@ -3,7 +3,16 @@ package com.example.geoserver.repository;
 import com.example.geoserver.entity.Signalement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 @Repository
 public interface SignalementRepository extends JpaRepository<Signalement, Long> {
-}
+
+    @Query("SELECT s FROM Signalement s " +
+           "JOIN s.statuts st " +
+           "WHERE st.dateStatut = (" +
+           "   SELECT MAX(st2.dateStatut) FROM StatutSignalement st2 WHERE st2.signalement = s" +
+           ") AND st.statut.id > 2")
+    List<Signalement> findAllValide();
